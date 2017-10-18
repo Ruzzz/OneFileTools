185ae3243a89d93da517e8a2d28f5fb177a62c3c
Ruslan Zaporojets <ruzzzua@gmail.com>
2017-10-19 02:07:50 +0300

    v1.2

--- cmd/VC++ Clean LIB.cmd
+++
@@ -1,5 +0,0 @@
-cd /d %1 || goto :EOF
-del /f /s /q /A:H *.suo
-del /f /s /q *.tlog *.lastbuildstate *.obj *.vc.db *.pch *.iobj *.ipdb *.ipch *.sdf *.log unsuccessfulbuild *.ilk *.tlog
-for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do rd "%%d"
-:EOF
