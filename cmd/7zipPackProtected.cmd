8659449f9e8b46edcc7ac537a501514010b54d12
Ruslan Zaporojets <ruzzzua@gmail.com>
2017-10-19 13:26:16 +0300

    7z

---
+++ cmd/7zipPackProtected.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+if "%~1"=="" (
+    echo ERROR: No Parameters
+    pause
+    goto :EOF
+)
+
+set /p PASS=Password: 
+
+:LOOP
+echo.
+echo ------------------------------
+echo Process: %1
+echo ------------------------------
+@echo on
+
+7za a -y -t7z -mx9 -mhe=on -mtc=on -m0=LZMA2 -p%PASS% "%~f1.7z" %1
+
+@echo off
+if not %ERRORLEVEL%==0 (
+    echo Error Code %ERRORLEVEL% for %1
+    pause
+    goto :EOF
+)
+shift
+if "%~1"=="" goto :EOF
+goto :LOOP
