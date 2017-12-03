c522b38465df4f5ab69c4ef0d4ba491f5549d1c8
Ruslan Zaporojets <ruzzzua@gmail.com>
2017-12-04 01:23:38 +0200

    fix FFmpeg_ExtractYouTubeAudio.cmd

--- cmd/FFmpeg_ExtractYouTubeAudio.cmd
+++ cmd/FFmpeg_ExtractYouTubeAudio.cmd
@@ -7,15 +7,9 @@ if (%1)==() goto :NO_PARAMETER
 
 :LOOP
 @echo on
-
-ffmpeg -i %1 -bsf:a aac_adtstoasc -c:a copy "%~dpn1_%random%.aac"
-
+ffmpeg -i %1 -bsf:a aac_adtstoasc -vn -c:a copy "%~dpn1_%random%.m4a"
 @echo off
-if not %ERRORLEVEL%==0 (
-    echo Error Code %ERRORLEVEL% for %1
-    pause
-    goto :EOF
-)
+if errorlevel 1 goto :ERROR
 shift
 if (%1)==() goto :EOF
 goto :LOOP
@@ -23,4 +17,8 @@ goto :LOOP
 :NO_PARAMETER
 echo Usage:
 echo this.bat FILE [FILE ...]
+pause
+goto :EOF
+
+:ERROR
 pause
