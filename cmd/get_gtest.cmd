815955dabccc7ea938ba620a5d233cd69c067667
Ruslan Zaporojets <ruzzzua@gmail.com>
2017-12-28 13:39:58 +0200

    fix get_test

--- cmd/get_gtest.cmd
+++ cmd/get_gtest.cmd
@@ -1,5 +1,5 @@
-:: git clone --depth 1 https://github.com/google/googletest.git
+::git clone --depth 1 https://github.com/google/googletest.git
 if exist gtest rd /q /s gtest
 mkdir gtest
-python2 ./googletest/googletest/scripts/fuse_gtest_files.py .
-:: rd /q /s googletest
+call python2 ./googletest/googletest/scripts/fuse_gtest_files.py .
+rd /q /s googletest
