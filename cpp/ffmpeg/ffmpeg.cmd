90decdeea7ff45b81edbac908500fce415493e39
Ruslan Zaporojets <ruzzzua@gmail.com>
2018-01-15 08:26:31 +0200

    fix ffmpeg make

--- cpp/ffmpeg/ffmpeg.cmd
+++ cpp/ffmpeg/ffmpeg.cmd
@@ -11,8 +11,9 @@
 :: PARAMS
 set __IN_DIR__=%DEV_LIBS_DISTR%\ffmpeg
 set __OUT_DIR__=%DEV_LIBS%/ffmpeg
-set __PLATFORM__=x32
-set __H264_DIR__=%DEV_LIBS%/x264
+set __PLATFORM__=x64
+set __H264_INCLUDE__=%DEV_LIBS%/x264/include
+set __H264_LIB__=%DEV_LIBS%/x264/lib
 set __ASM__=0
 
 
@@ -21,17 +22,17 @@ if "%__PLATFORM__%" == "x32" (
     set __CL_PLATFORM___=x86
     set __MINGW_PLATFORM___=MINGW32
     set __OUT_DIR__=%__OUT_DIR__%/x32
-    set __H264_DIR__=%__H264_DIR__%/x32
+    set __H264_LIB__=%__H264_LIB__%32
 ) else (
     set __CL_PLATFORM___=amd64
     set __MINGW_PLATFORM___=MINGW64
     set __OUT_DIR__=%__OUT_DIR__%/x64
-    set __H264_DIR__=%__H264_DIR__%/x64
+    set __H264_LIB__=%__H264_LIB__%64
 )
 if "%__ASM__%" == "1" (
     set __SH_PARAMS__=%__SH_PARAMS__% --asm
     set __OUT_DIR__=%__OUT_DIR__%_asm
-    set __H264_DIR__=%__H264_DIR__%_asm
+    set __H264_LIB__=%__H264_LIB__%_asm
 )
 
 
@@ -61,5 +62,5 @@ set MSYSTEM=%__MINGW_PLATFORM___%
 
 
 :: COMPILE
-%MSYS2_ROOT%\usr\bin\bash.exe --login -x %~dpn0.sh %__SH_PARAMS__% --in %__IN_DIR__% --out %__OUT_DIR__% --h264 %__H264_DIR__%
+%MSYS2_ROOT%\usr\bin\bash.exe --login -x %~dpn0.sh %__SH_PARAMS__% --in %__IN_DIR__% --out %__OUT_DIR__% --h264-include %__H264_INCLUDE__% --h264-lib %__H264_LIB__%
 pause
--- cpp/ffmpeg/ffmpeg.sh
+++ cpp/ffmpeg/ffmpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE: this.sh [--asm] [--x32] --in IN_DIR --out OUT_DIR --h264 H264_DIR
+# USAGE: this.sh [--asm] [--x32] --in IN_DIR --out OUT_DIR --h264-include H264_INCLUDE --h264-lib H264_LIB
 
 
 # Default params
@@ -8,7 +8,8 @@ PLATFORM=x86_64  # or amd64
 TARGET_OS=win64
 IN_DIR=
 OUT_DIR=
-H264_DIR=
+H264_INCLUDE=
+H264_LIB=
 
 
 # Parse params
@@ -22,8 +23,11 @@ while [ "$1" != "" ]; do
                         ;;
         -a | --asm )    NO_ASM_OPT=
                         ;;
-        --h264 )        shift
-                        H264_DIR=${1//\\//}
+        --h264-include ) shift
+                        H264_INCLUDE=${1//\\//}
+                        ;;
+        --h264-lib )    shift
+                        H264_LIB=${1//\\//}
                         ;;
         --x32 )         PLATFORM=x86_32  # or x86
                         TARGET_OS=win32
@@ -40,6 +44,7 @@ cd $IN_DIR
 # if false; then
 # TODO: --disable-everything --disable-swresample --disable-avdevice --disable-avformat --disable-postproc --disable-avfilter
 CC=cl ./configure \
+    --disable-everything \
     --prefix=$OUT_DIR \
     --toolchain=msvc \
     --enable-gpl \
@@ -52,9 +57,9 @@ CC=cl ./configure \
     --enable-libx264 \
     --enable-decoder=h264 \
     --enable-encoder=libx264 \
-    --extra-cflags="-MT -I$H264_DIR/include -DNO_PREFIX -fPIC" \
-    --extra-cflags="-MT -I$H264_DIR/include -DNO_PREFIX -fPIC" \
-    --extra-ldflags="-LIBPATH:$H264_DIR/lib"
+    --extra-cflags="-MT -I$H264_INCLUDE -DNO_PREFIX -fPIC" \
+    --extra-cflags="-MT -I$H264_INCLUDE -DNO_PREFIX -fPIC" \
+    --extra-ldflags="-LIBPATH:$H264_LIB"
 # fi
 
 make clean
